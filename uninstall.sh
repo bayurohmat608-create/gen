1f98c8cf27df667af8dbbee1e98566f5dc4610cc
bayurohmat608-create <bayurohmat608@gmail.com>
2026-09-20 11:54:43 +0700

    Remove web launcher on uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -eu
 rm -rf "${HOME}/.local/share/blind-agent-room"
-rm -f "${HOME}/.local/bin/blindroom"
+rm -f "${HOME}/.local/bin/blindroom" "${HOME}/.local/bin/blindroom-web"
 echo "Blind Agent Room removed."
