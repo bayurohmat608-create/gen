5b72c48707ab092cca0a8b24168426efe50b1e30
bayurohmat608-create <bayurohmat608@gmail.com>
2026-09-21 17:16:23 +0700

    Temporary CP86 transport resolver: droide-resolver/build.gradle.kts

---
+++ droide-resolver/build.gradle.kts
@@ -0,0 +1,7 @@
+// CP86 dependency resolver sync 3
+plugins {
+    id("com.android.application") version "8.13.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.0" apply false
+}
